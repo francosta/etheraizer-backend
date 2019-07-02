7b63cb3e72cb965bf0eb5cc56aafafb814f02a02
Francisco Costa <francisco@fcosta.pt>
2019-07-02 17:11:32 +0100

    Added function to create SupportContract in the backedn once a contribution has been made.

--- app/controllers/support_contracts_controller.rb
+++ app/controllers/support_contracts_controller.rb
@@ -1,2 +1,18 @@
 class SupportContractsController < ApplicationController
+
+    def index
+        support_contracts = SupportContract.all
+        render json: support_contracts
+    end
+
+    def create
+        support_contract = SupportContract.create(support_contract_params)
+        render json: support_contract
+    end
+
+    private
+
+    def support_contract_params
+        params.permit(:user_id, :project_id)
+    end
 end
