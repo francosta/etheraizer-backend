b769b86e1e9bc7b34a02d45b4616b7a2d3d3a4b4
francosta <francisco@fcosta.pt>
2019-07-08 17:54:16 +0100

    Added profile picture to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_08_145356) do
+ActiveRecord::Schema.define(version: 2019_07_08_165249) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_07_08_145356) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_picture"
   end
 
 end
